ef757bcc00929b29bb711f31c090507e315b5c46
kadeshiro <gaaadaaa16@gmail.com>
2020-07-03 07:29:52 +0900

    [remove] temp folder

--- packages/test-mock/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  automock: true,
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|js)'],
-  transform: { '^.+\\.ts$': 'ts-jest' },
-  unmockedModulePathPatterns: ['ts-jest/utils'],
-};
--- packages/test-mock/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { sum } from './module';
-
-export const main = (func: typeof sum): number => func(1, 2);
